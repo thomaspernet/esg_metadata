21d8a4e65c547e31c938a6a770545c5f52e88b72
Thomas <t.pernetcoudrier@gmail.com>
2021-07-12 18:40:23 +0200

    create true stars and clean up division by 0
    230_esg_metadata

--- 01_data_preprocessing/00_download_data/METADATA_TABLES/esg_metaanalysis.py
+++ 01_data_preprocessing/00_download_data/METADATA_TABLES/esg_metaanalysis.py
@@ -52,19 +52,40 @@ var = (
         id = lambda x: x['id'].ffill().astype(int),
         n = lambda x: x['n'].apply(pd.to_numeric, errors='coerce'),
         sr = lambda x: x['sr'].apply(pd.to_numeric, errors='coerce'),
+        beta = lambda x: x['beta'].apply(pd.to_numeric, errors='coerce'),
         true_standard_error = lambda x: x['true_standard_error'].apply(pd.to_numeric, errors='coerce'),
         true_t_value = lambda x: x['true_t_value'].apply(pd.to_numeric, errors='coerce'),
-        true_stars = lambda x: np.where(~x['true_stars'].isin(['1_PERCENT','5_PERCENT','10_PERCENT']), np.nan, x['true_stars']),
-        significant = lambda x: np.where(x['true_stars'].isin(['1_PERCENT','5_PERCENT','10_PERCENT']), "YES", "NO")
+        true_critical = lambda x: x['beta']/x['true_standard_error']
+        #significant = lambda x: np.where(x['true_stars'].isin(['1_PERCENT','5_PERCENT','10_PERCENT']), "YES", "NO")
         )
         .loc[lambda x: ~x["table_refer"].isin([np.nan])]
         .drop(columns = ['image', 'ID2'])
         .replace(',', '', regex=True)
         .replace('\"', ' ', regex=True)
         .replace('\n', ' ', regex=True)
-        .replace('#N/A|#VALUE!|\?\?\?', np.nan, regex=True)
+        .replace('#N/A|#VALUE!|\?\?\?|#DIV/0!', np.nan, regex=True)
 )
 
+# using true critical value
+var.loc[var['true_critical'] >=2.58, 'true_stars'] = "1_PERCENT"
+var.loc[(var['true_critical'] >=1.96) & (var['true_critical'] <2.58), 'true_stars'] = "5_PERCENT"
+var.loc[(var['true_critical'] >=1.645) & (var['true_critical'] <1.96), 'true_stars'] = "10_PERCENT"
+
+# using true t-value
+var.loc[(var['true_t_value'] >=2.58) & (~var['true_stars'].isin(['1_PERCENT','5_PERCENT','10_PERCENT'])), 'true_stars'] = "1_PERCENT"
+var.loc[(var['true_t_value'] >=1.96) & (var['true_t_value'] <2.58) & (~var['true_stars'].isin(['1_PERCENT','5_PERCENT','10_PERCENT'])), 'true_stars'] = "5_PERCENT"
+var.loc[(var['true_t_value'] >=1.645) & (var['true_t_value'] <1.96) & (var['true_stars'].isin(['1_PERCENT','5_PERCENT','10_PERCENT'])), 'true_stars'] = "10_PERCENT"
+
+
+var = var.assign(
+significant = lambda x: np.where(x['true_stars'].isin(['1_PERCENT','5_PERCENT','10_PERCENT']), "YES", "NO")
+)
+
+
+#var.loc[lambda x:
+#(x['to_remove'].isin(['TO_KEEP'])) & (x['to_check_final'].isin(['NO']))
+#].loc[lambda x: x['true_standard_error'].isin([np.nan])][
+#['id','beta', 'sr', 'true_standard_error', 'test_standard_error']]
 # READ DATA
 # SAVE LOCALLY
 input_path = os.path.join(parent_path, "00_data_catalog",
@@ -120,7 +141,11 @@ schema = [
 {'Name': 'adjusted_dependent', 'Type': 'string', 'Comment': 'reorganise dependent variable into smaller groups'},
 {'Name': 'adjusted_independent', 'Type': 'float', 'Comment': 'reorganise independent variable into smaller group'},
 {'Name': 'adjusted_model', 'Type': 'string', 'Comment': 'reorganise model variable into smaller group'},
-{'Name': 'significant', 'Type': 'string', 'Comment': 'is beta significant'}
+{'Name': 'significant', 'Type': 'string', 'Comment': 'is beta significant'},
+{'Name': 'to_check_final', 'Type': 'string', 'Comment': 'Final check rows'},
+{'Name': 'critical_99', 'Type': 'string', 'Comment': '99 t stat critical value calculated based on nb obserations'},
+{'Name': 'critical_90', 'Type': 'string', 'Comment': '90 t stat critical value calculated based on nb obserations'},
+{'Name': 'critical_95', 'Type': 'string', 'Comment': '95 t stat critical value calculated based on nb obserations'},
 ]
 
 # ADD DESCRIPTION
