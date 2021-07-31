c2a65360c90ef52fcb71ea0fb9186850d069eb7d
Thomas <t.pernetcoudrier@gmail.com>
2021-07-31 10:42:09 +0200

    add make_data_schema_github
    238_esg_metadata

--- utils/create_schema.py
+++ utils/create_schema.py
@@ -13,6 +13,83 @@ from diagrams.aws.storage import S3
 
 #create_schema(path_json = path_json)
 #path_save_image = os.getcwd()
+path = os.getcwd()
+def make_data_schema_github(name_json):
+    """
+    path: current cwd
+    parameters: json path to parameters_ETL_PROJECT.json
+    """
+    path_json = os.path.join(str(Path(path).parent.parent), 'utils',name_json)
+    with open(path_json) as json_file:
+        parameters = json.load(json_file)
+    README = """
+# Data Catalogue
+
+{}
+
+    """
+
+    top_readme = """
+
+## Table of Content
+
+    """
+
+    template = """
+
+## Table {0}
+
+- Database: {1}
+- S3uri: `{2}`
+- Partitition: {3}
+- Script: {5}
+
+{4}
+
+    """
+    github_link = os.path.join("https://github.com/", parameters['GLOBAL']['GITHUB']['owner'],
+                               parameters['GLOBAL']['GITHUB']['repo_name'], "tree/master/00_data_catalog#table-")
+    for key, value in parameters['TABLES'].items():
+        if key == 'CREATION':
+            param = 'ALL_SCHEMA'
+        else:
+            param = 'STEPS'
+
+        for schema in parameters['TABLES'][key][param]:
+            description = schema['description']
+            DatabaseName = schema['metadata']['DatabaseName']
+            target_S3URI = schema['metadata']['target_S3URI']
+            partition = schema['partition_keys']
+            script = schema['metadata']['github_url']
+
+            if param =='ALL_SCHEMA':
+                table_name_git = '{}{}'.format(
+                    schema['metadata']['TablePrefix'],
+                    os.path.basename(schema['metadata']['target_S3URI']).lower()
+                )
+            else:
+                try:
+                    table_name_git = schema['metadata']['TableName']
+                except:
+                    table_name_git = '{}{}'.format(
+                    schema['metadata']['TablePrefix'],
+                    os.path.basename(schema['metadata']['target_S3URI']).lower()
+                )
+
+            tb = pd.json_normalize(schema['schema']).to_markdown()
+            toc = "{}{}".format(github_link, table_name_git)
+            top_readme += '\n- [{0}]({1})'.format(table_name_git, toc)
+
+            README += template.format(table_name_git,
+                                      DatabaseName,
+                                      target_S3URI,
+                                      partition,
+                                      tb,
+                                      script
+                                      )
+    README = README.format(top_readme)
+    with open(os.path.join(str(Path(path).parent.parent), '00_data_catalog/README.md'), "w") as outfile:
+        outfile.write(README)
 
 def generate_graph_etl(data, list_final, path_save_image):
     """
